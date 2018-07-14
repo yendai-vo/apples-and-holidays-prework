4a679ddaa5424270fe95f405718bd19456adfc77
Cindy Vo <yendai.vo@hotmail.com>
2018-07-14 19:08:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,6 +55,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  holiday_hash.each { |key, value|
+    puts "#{key.capitalize}:"
+    value.each { |key2, val2|
+      puts "#{key2}: #{val2}"
+    }
+  }
 end
 
 def all_holidays_with_bbq(holiday_hash)
